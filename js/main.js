f16e4baf274c410268809b039b5a305cf58bd2f2
Anita <a.nita@hotmail.co.uk>
2015-07-28 17:32:48 +0100

    search works

--- js/main.js
+++ js/main.js
@@ -1,1 +1,60 @@
+(function() {
+
+
+    var id = cloud_api;
+    var cloudPlayer = new CloudPlayer(id);
+
+    $(document).ready(function () {
+        console.log('workin');
+        var result = document.querySelector('#result');
+        var searchInput = document.querySelector('#searchInput');
+        var buttonSearch = document.querySelector('#buttonSearch');
+
+
+        buttonSearch.addEventListener("click", function() {
+            result.innerHTML= "";
+
+            cloudPlayer.searchList(searchInput.value, function(list){
+                if(!list.length){
+                    var li = document.createElement('li');
+                    li.innerHTML = "No songs match your search query";
+                    li.addEventListener("click", function(){console.log("testing")});
+                    result.appendChild(li);
+                }
+                list.forEach(function(song){
+                    if(song.streamable){
+                        var li = document.createElement('li');
+                        li.innerHTML = song.title;
+                        li.setAttribute('id', song.id);
+                        result.appendChild(li);
+                    };
+                });
+            });
+        });
+
+    });
+
+
+
+
+//player constructor with methods
+    function CloudPlayer(api_key){
+        SC.initialize({
+            client_id: api_key
+        });
+
+        this.searchList = function(str, callback) {
+            SC.get('/tracks', { q: str }, function(tracks) {
+                callback(tracks);
+            });
+        }
+    }
+
+}());
+
+
+
+
+
+
 
